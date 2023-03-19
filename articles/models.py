9b72141a814161dd4b9b6d83d382839e6869c9e6
Nauman Ahmed <mcsip@hotmail.com>
2023-03-19 20:32:03 +0500

    c

--- articles/models.py
+++ articles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Article(models.Model):
@@ -6,3 +7,5 @@ class Article(models.Model):
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    publish = models.DateField(auto_now_add=False, auto_now=False, default=timezone.now) 
+    # null=True for database to allow null, blank=True for form to allow empty, non mandatory
