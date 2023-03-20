615a4284dc975116c7705c7c19fb5ae0a2cb49c6
Nauman Ahmed <mcsip@hotmail.com>
2023-03-20 15:43:45 +0500

    slug

--- articles/models.py
+++ articles/models.py
@@ -22,17 +22,25 @@ class Article(models.Model):
         # obj. save ()
         # do another something
 
+def slugify_instance_title(instance, save=False):
+    slug = slugify(instance.title)
+    qs = Article.objects.filter(slug=slug).exclude(id=instance.id) # exclude current instance. i.e like ignore(id) in update function
+    if qs.exists():
+        slug = f'{slug}-{qs.count()+1}'
+    instance.slug = slug
+    if save:
+        instance.save() # calling save because this is after save i.e post_save so we have to manually call
+    return instance # optional
 
 # this signal is alrternative to what we did above in save method
 def article_pre_save(sender, instance, *args, **kwargs):
     if instance.slug is None:
-        instance.slug = slugify(instance.title)
+        slugify_instance_title(instance, save=False)
 
 pre_save.connect(article_pre_save, sender=Article)
-
+ 
 def article_post_save(sender, instance, created, *args, **kwargs):
     if created:
-        instance.slug = slugify(instance.title)
-        instance.save() # calling save because this is after save i.e post_save so we have to manually call
+        slugify_instance_title(instance, save=True)
 
 post_save.connect(article_post_save, sender=Article)
